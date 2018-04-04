6c35257931e4180cee13e99262f4ce5a41e868bc
Ronel Dornas <ronel.dornas@ezdelivery.co>
2018-04-04 18:46:26 -0300

    first commit

---
+++ src/Deck.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import { View, Animated, PanResponder } from 'react-native';
+
+class Deck extends Component {
+    constructor(props) {
+        super(props);
+
+        const position = new Animated.ValueXY()
+        const panResponder = PanResponder.create({
+            onStartShouldSetPanResponder: () => true,
+            onPanResponderMove: (event, gesture) => {
+                position.setValue({ x: gesture.dx, y: gesture.dy })
+            },
+            onPanResponderRelease: () => {}
+        });
+
+        this.state = { panResponder, position };
+    }
+    renderCards() {
+        return this.props.data.map((item, index) => {
+            if(index === 0) {
+                return (
+                    <Animated.View
+                    {...this.state.panResponder.panHandlers}
+                    style={this.state.position.getLayout()}
+                    >
+                        {this.props.renderCard(item)}
+                    </Animated.View>
+                )
+            }
+            return this.props.renderCard(item)
+        }) 
+    }
+    
+    render() {
+        return(
+            <View >
+                {this.renderCards()}
+            </View>
+        )
+    }
+}
+
+export default Deck;
